e88bf902336c6eea86bfc6df8a939e7804f7c8d8
Rosellyne <elijahroselyne@gmail.com>
2019-06-03 00:03:23 +0300

    add the new quotes to quote component content

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
+  quotes = [
+    new Quote( 'Jackson brown', 'Rosie', 'success is getting what you want ,Happiness is liking what you get.'),
+    new Quote('John washington', 'Rosie', 'You only live once,but if you do it right,once is enough')
+  ];
 
   constructor() { }
 
